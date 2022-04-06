c67e7a017d153ec2630ceeed2141bafb46c4e803
Tonmoy Roy <rxdevs100@gmail.com>
2022-04-07 01:15:51 +0600

    note add, edit, delete features added with localStorage

--- src/Components/Diary.js
+++ src/Components/Diary.js
@@ -1,17 +1,74 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Diary = () => {
-    const [notes, setNotes] = useState();
+    const [notes, setNotes] = useState([]);
+    const [isEditing, setEditing] = useState(false);
     const newNoteRef = useRef(null);
+    const getNotesFromStorage = () => JSON.parse(localStorage.getItem('notes'));
+    const addNotesToStorage = (notes) => localStorage.setItem("notes", JSON.stringify(notes))
+    useEffect(() => {
+        const getNotes = getNotesFromStorage();
+        if (getNotes) {
+            setNotes(getNotes);
+        }
+    }, []);
     const addNote = () => {
-        setNotes(newNoteRef.current.value);
+        const note = {
+            // id: newNoteRef.current.value.length,
+            text: newNoteRef.current.value,
+        }
+        const storeNotes = [note, ...notes];
+        setNotes(storeNotes);
+        newNoteRef.current.value = "";
+        addNotesToStorage(storeNotes);
     }
-    
+    const editNote = () => setEditing(true)
+    const removeNote = (text) => {
+        const filteredNotes = notes.filter(note => note.text !== text);
+        setNotes(filteredNotes);
+        addNotesToStorage(filteredNotes);
+    }
+    const cancelToEdit = () => setEditing(false);
+    const updateNote = (text) => {
+        notes.find(note => note.text === text && (note.text = newNoteRef.current.value, true));
+        addNotesToStorage(notes);
+        setEditing(false);
+        newNoteRef.current.value = "";
+    }
     return (
         <div>
             <input type="text" placeholder="Write your note here" ref={newNoteRef} />
             <button onClick={addNote}>Add to Diary</button>
+            <div>
+                {
+                    notes.map((note) =>
+                        (
+                            <div key={note.text}>
+                                {
+                                    !isEditing && (
+                                        <div>
+                                            <p>{note.text}</p>
+                                            <button onClick={editNote}>Edit</button>
+                                            <button onClick={() => removeNote(note.text)}>Delete</button>
+                                        </div>
+                                    )
+                                }
+                                {
+                                    isEditing && (
+                                        <div>
+                                            <input type="text" placeholder="Write your note here" ref={newNoteRef} />
+                                            <button onClick={cancelToEdit}>Cancel</button>
+                                            <button onClick={() => updateNote(note.text)}>Save</button>
+                                        </div>
+                                    )
+                            
+                                }
+                            </div>
+                            )
+                    )
+                }
+            </div>
         </div>
-    )
+    );
 }
 export default Diary;
